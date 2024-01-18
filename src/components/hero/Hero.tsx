5a4df2fa249650ccb57190992425838005755c0c
Subham <jainofcsubham597@gmail.com>
2024-01-19 02:59:57 +0530

    feat: Added links

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import "./Hero.css"
 
 export const Hero = () => {
+
+  const navigate = useNavigate()
+
+  const goToCalculator = () => {
+    navigate("/dashboard")
+  }
+
   return (
     <>
       <div className="hero_container">
         <h1 className="hero_text">Calculate Your Carbon Footprint</h1>
         <div className="hero_action">
-          <div className="hero_action_item get_started">Get Started</div>
+          <div className="hero_action_item get_started" onClick={goToCalculator}>Get Started</div>
           <div className="hero_action_item ">Learn More</div>
         </div>
       </div>
